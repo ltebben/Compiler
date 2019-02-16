3078c42d59274d784325c355f087d667920e8953
Laura Tebben <tebbenla@mail.uc.edu>
2019-02-16 14:40:22 -0500

    progress

--- scanproto2.py
+++ scanproto2.py
@@ -1,6 +1,7 @@
 class Scanner():
+    lineno = 0
+
     def __init__(self):
-        self.lineno = 0
         self.types = {
             '\"': "String",
             '/': "CommentDivide",
@@ -35,7 +36,7 @@ class Scanner():
                 'not','return','true','false'])
 
     def readFile(self):
-        with open('testPgms/correct/test1.src', 'r') as r:
+        with open('testPgms/correct/test2.src', 'r') as r:
             for line in r:
                 self.lineno += 1
                 line = line.lower()
@@ -63,97 +64,99 @@ class Scanner():
     def scan(self):
         c = self.nextchar()
         while True:
-            if self.nextChar:
-                token = self.nextChar
-                self.nextChar = ''
-            else:
-                token = next(c)
-
-            while token in self.ignore:
-                token = next(c)
-
-            if token in self.types:
-                if self.types[token] == 'String':
-                    token += next(c)
-                    while token[-1] != '"':
-                        token += next(c)
-                    yield ('String', token)
-
-                elif self.types[token] == 'CommentDivide':
-                    tmp = next(c)
-
-                    if tmp == '*':
-                        # Found block comment -- erase
-                        token = self.eraseBlockComment(c)
-                    elif tmp == '/':
-                        # Found line comment -- erase
-                        while token[-1] != '\n':
-                            token += next(c)
-                    elif tmp.isdigit():
-                        # Found division -- save digit for later
-                        self.nextChar = tmp
-                    # TODO: Keep processing for next real token
-                    continue
-
-                elif self.types[token] == 'Relation':
-                    tmp = next(c)
-                    if tmp.isdigit():
-                        self.nextChar = tmp
-                    elif tmp == "=":
-                        token += tmp
-                    else:
-                        pass
-                        # TODO: error
-                    yield ('Relation', token)
-
-                elif self.types[token] == 'AssignmentIndex':
-                    tmp = next(c)
-
-                    if tmp == "=":
-                        yield ('Assignment', token+tmp)
-                    elif tmp.isdigit():
-                        yield('Index', token)
-                        self.nextChar = tmp
-                    else:
-                        pass
-                        # TODO: error
-
+            try:
+                if self.nextChar:
+                    token = self.nextChar
+                    self.nextChar = ''
                 else:
-                    yield (self.types[token], token)
+                    token = next(c)
 
-            else:
-                if token.isdigit():
-                    # TODO: enforce [0-9][0-9_]*[.[0-9]*]
-                    tmp = next(c)
-                    if not tmp.isdigit():
-                        while tmp == '_':
-                            tmp = next(c)
-                        if not tmp.isdigit() or tmp!='_':
+                while token in self.ignore:
+                    token = next(c)
+
+                if token in self.types:
+                    if self.types[token] == 'String':
+                        token += next(c)
+                        while token[-1] != '"':
+                            token += next(c)
+                        yield ('String', token)
+
+                    elif self.types[token] == 'CommentDivide':
+                        tmp = next(c)
+
+                        if tmp == '*':
+                            # Found block comment -- erase
+                            token = self.eraseBlockComment(c)
+                        elif tmp == '/':
+                            # Found line comment -- erase
+                            while token[-1] != '\n':
+                                token += next(c)
+                        elif tmp.isdigit():
+                            # Found division -- save digit for later
+                            self.nextChar = tmp
+                        # TODO: Keep processing for next real token
+                        continue
+
+                    elif self.types[token] == 'Relation':
+                        tmp = next(c)
+                        if tmp.isdigit():
+                            self.nextChar = tmp
+                        elif tmp == "=":
+                            token += tmp
+                        else:
+                            pass
+                            # TODO: error
+                        yield ('Relation', token)
+
+                    elif self.types[token] == 'AssignmentIndex':
+                        tmp = next(c)
+
+                        if tmp == "=":
+                            yield ('Assignment', token+tmp)
+                        elif tmp.isdigit():
+                            yield('Index', token)
+                            self.nextChar = tmp
+                        else:
                             pass
                             # TODO: error
 
-                    while not (tmp in self.ignore or tmp in self.stop):
-                        token += tmp
-                        tmp = next(c)
-                    self.nextChar = tmp
-
-                    yield ('Digit', token)
-                else:
-                    tmp = next(c)
-
-                    while not (tmp in self.ignore or tmp in self.stop):
-                        token += tmp
-                        tmp = next(c)
-
-                    self.nextChar = tmp
-
-                    if token in self.reserved:
-                        yield ('Keyword', token)
                     else:
-                        yield ('Identifier', token)
+                        yield (self.types[token], token)
+
+                else:
+                    if token.isdigit():
+                        # TODO: enforce [0-9][0-9_]*[.[0-9]*]
+                        tmp = next(c)
+                        if not tmp.isdigit():
+                            while tmp == '_':
+                                tmp = next(c)
+                            if not tmp.isdigit() or tmp!='_':
+                                pass
+                                # TODO: error
+
+                        while not (tmp in self.ignore or tmp in self.stop):
+                            token += tmp
+                            tmp = next(c)
+                        self.nextChar = tmp
+
+                        yield ('Digit', token)
+                    else:
+                        tmp = next(c)
+
+                        while not (tmp in self.ignore or tmp in self.stop):
+                            token += tmp
+                            tmp = next(c)
+
+                        self.nextChar = tmp
+
+                        if token in self.reserved:
+                            yield ('Keyword', token)
+                        else:
+                            yield ('Identifier', token)
+
+            except StopIteration:
+                yield ('EOF', 'eof')
+                break
 
 
-s = Scanner()
-gen = s.scan()
-while True:
-    print(next(gen))
+
